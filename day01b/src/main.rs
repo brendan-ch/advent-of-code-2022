ce94709a037a6f103ea7b954c577447ade2c8e73
Brendan Chen <smallpigeon1@gmail.com>
2022-11-30 21:11:40 -0800

    add day 1

--- day01b/src/main.rs
+++ day01b/src/main.rs
@@ -1,17 +1,26 @@
 use std::time::Instant;
 
 fn main() {
-    let input = include_str!("../input.txt");
+    let input = include_str!("../input.txt").to_owned() + "\n";
     let now = Instant::now();
 
     println!("Hello, world!");
 
     let mut most = 0;
+    let mut second_most = 0;
+    let mut third_most = 0;
     let mut current = 0;
     for line in input.split('\n') {
         if line.is_empty() {
             if current > most {
+                third_most = second_most;
+                second_most = most;
                 most = current;
+            } else if current > second_most {
+                third_most = second_most;
+                second_most = current;
+            } else if current > third_most {
+                third_most = current;
             }
 
             current = 0;
@@ -20,7 +29,8 @@ fn main() {
         }
     }
 
-    println!("{most}");
+    let result = most + second_most + third_most;
+    println!("{result}");
 
     let elapsed = now.elapsed().as_micros();
     println!("Time elapsed: {elapsed} microseconds");
