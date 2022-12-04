85756173dbc4ac22c9a83c2f4041cbeebcae1203
Brendan Chen <smallpigeon1@gmail.com>
2022-12-03 22:20:47 -0800

    learned how to use iterators properly

--- day04b/src/main.rs
+++ day04b/src/main.rs
@@ -4,34 +4,28 @@ fn main() {
     let now = Instant::now();
     let input = include_str!("../input.txt");
 
-    // In how many assignment pairs does one range fully contain the other?
-    let mut count = 0;
+    // In how many assignment pairs does one range overlap the other
+    let mut t = 0;
 
-    for line in input.split('\n') {
-        let mut split_line = line.split(',');
-        let mut first_pair_iterator = split_line.next().unwrap()
-            .split('-');
-        let mut second_pair_iterator = split_line.next().unwrap()
-            .split('-');
+    let mut n = [0, 0, 0, 0];
+    let mut c = 0;
+    
+    input.split(&[',', '-', '\n'])
+        .for_each(|x| {
+            n[c] = x.parse::<i32>().unwrap();
+            
+            c += 1;
+            if c >= 4 {
+                // Do some counting
+                if (n[1] - n[2] >= 0 && n[0] - n[3] < 0)
+                || (n[1] - n[2] > 0 && n[0] - n[3] <= 0) {
+                    t += 1;
+                }
 
-        let first_pair = (
-            first_pair_iterator.next().unwrap().parse::<i32>().unwrap(),
-            first_pair_iterator.next().unwrap().parse::<i32>().unwrap(),
-        );
-        
-        let second_pair = (
-            second_pair_iterator.next().unwrap().parse::<i32>().unwrap(),
-            second_pair_iterator.next().unwrap().parse::<i32>().unwrap(),
-        );
-
-        if (first_pair.1 - second_pair.0 >= 0 && first_pair.0 - second_pair.1 < 0)
-        || (first_pair.1 - second_pair.0 > 0 && first_pair.0 - second_pair.1 <= 0)
-        {
-            count += 1;
-        }
-
-    }
-    println!("{count}");
+                c = 0;
+            }
+        });
+    println!("{t}");
 
     let elapsed = now.elapsed().as_micros();
     println!("Time elapsed: {elapsed} microseconds");
