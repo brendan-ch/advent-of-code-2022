48161e7c7b2b0ddbcc0acec9edb7a924a1e1ac9c
Brendan Chen <smallpigeon1@gmail.com>
2022-12-04 22:53:08 -0800

    did some light refactoring

--- day05b/src/main.rs
+++ day05b/src/main.rs
@@ -5,41 +5,38 @@ fn main() {
     let input = include_str!("../input.txt");
 
     let mut stacks: Vec<VecDeque<&str>> = vec![VecDeque::new(); 9];
-    let mut instructions = false;
 
-    for line in input.split('\n') {
-        if instructions {
-            // do some light reading
-            let mut n: [usize; 3] = [0, 0, 0];
-            let iterator = line.split(' ').filter(|x| x.parse::<i32>().is_ok());
-            let mut c = 0;
-            iterator.for_each(|x| {
-                n[c] = x.parse::<usize>().unwrap();
-                c += 1;
-            });
+    let mut split = input.split("\n\n");
+    for line in split.next().unwrap().split('\n') {
+        // read the original layout
+        // find the index of each letter
+        line.match_indices(char::is_alphabetic).for_each(|x| {
+            let stack_index = (x.0 - 1) / 4;
 
+            stacks[stack_index].push_back(x.1);
+        });
+    }
+
+    // do some light reading
+    let mut n: [usize; 3] = [0, 0, 0];
+    let iterator = split.next().unwrap().split(&[' ', '\n']);
+    let mut c = 0;
+    iterator.for_each(|x| {
+        if x.parse::<usize>().is_ok() {
+            n[c] = x.parse::<usize>().unwrap();
+            c += 1;
+        }
+
+        if c >= 3 {
             for x in (0..n[0]).rev() {
                 // move some boxes
                 let to_move = stacks[n[1] - 1].remove(x as usize).unwrap();
                 stacks[n[2] - 1].push_front(to_move);
             }
 
-        } else {
-            // read the original layout
-            // find the index of each letter
-            line.match_indices(char::is_alphabetic)
-                .for_each(|x| {
-                    let stack_index = (x.0 - 1) / 4;
-
-                    stacks[stack_index].push_back(x.1);
-                });
-
-            if line.is_empty() {
-                // move on to reading the instructions
-                instructions = true;
-            }
+            c = 0;
         }
-    }
+    });
 
     // Print some characters
     stacks.iter()
